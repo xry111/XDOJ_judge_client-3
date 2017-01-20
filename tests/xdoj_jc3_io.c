5f0ba842deeb2d5d45a9ade89799c6b1421ac07c
Xi Ruoyao <xry111@outlook.com>
2017-01-20 17:38:35 +0800

    Add Copyright information

--- tests/xdoj_jc3_io.c
+++ tests/xdoj_jc3_io.c
@@ -1,3 +1,22 @@
+/* xdoj_jc3_io.c : file I/O testing.
+   Copyright (C) 2017 Lab of ACM/ICPC, Xidian University.
+
+This file is part of XDOJ Judge Client v3.
+
+XDOJ Judge Client v3 is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+XDOJ Judge Client v3 is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XDOJ Judge Client v3; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>.  */
+
 #include "xdoj_jc3_io.h"
 #include <stdio.h>
 
